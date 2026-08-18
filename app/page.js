5e09c28fda9f51473f3b4e2d62a506948e37b131
warisoncabral-rgb <warisoncabral@gmail.com>
2026-08-18 17:10:37 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-export default function Page(){return <h1>Contexto Ads MVP funcionando!</h1>}
+export default function Page() {
+  return (
+    <main>
+      <h1>Contexto Ads</h1>
+      <p>Plataforma em construção.</p>
+    </main>
+  )
+}
