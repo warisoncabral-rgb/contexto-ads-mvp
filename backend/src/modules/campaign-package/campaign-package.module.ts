b719632f115c92c6b295726fbcf2c3f3cdb6df48
warisoncabral-rgb <warisoncabral@gmail.com>
2026-08-26 18:09:30 -0300

    feat: wire persisted handoff to existing Execution Plan core

--- backend/src/modules/campaign-package/campaign-package.module.ts
+++ backend/src/modules/campaign-package/campaign-package.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../infrastructure/database/database.module';
+import { ExecutionPlanModule } from '../execution-plan/execution-plan.module';
 import { CampaignPackageController } from './campaign-package.controller';
+import { CampaignPackageHandoffService } from './campaign-package-handoff.service';
 import { CampaignPackageMapper } from './campaign-package.mapper';
 import { CampaignPackageService } from './campaign-package.service';
 
 @Module({
+  imports: [DatabaseModule, ExecutionPlanModule],
   controllers: [CampaignPackageController],
-  providers: [CampaignPackageService, CampaignPackageMapper],
-  exports: [CampaignPackageService, CampaignPackageMapper],
+  providers: [CampaignPackageService, CampaignPackageMapper, CampaignPackageHandoffService],
+  exports: [CampaignPackageService, CampaignPackageMapper, CampaignPackageHandoffService],
 })
 export class CampaignPackageModule {}
