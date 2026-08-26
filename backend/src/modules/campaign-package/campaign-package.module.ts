fd351dc4c0b4a351cd7ffca54060791e02d1031b
warisoncabral-rgb <warisoncabral@gmail.com>
2026-08-26 18:02:38 -0300

    feat: expose Campaign Package preparation mapper

--- backend/src/modules/campaign-package/campaign-package.module.ts
+++ backend/src/modules/campaign-package/campaign-package.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CampaignPackageController } from './campaign-package.controller';
+import { CampaignPackageMapper } from './campaign-package.mapper';
 import { CampaignPackageService } from './campaign-package.service';
 
 @Module({
   controllers: [CampaignPackageController],
-  providers: [CampaignPackageService],
-  exports: [CampaignPackageService],
+  providers: [CampaignPackageService, CampaignPackageMapper],
+  exports: [CampaignPackageService, CampaignPackageMapper],
 })
 export class CampaignPackageModule {}
