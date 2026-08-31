e672c044cb8a721bd17c73f18b0df6ae15db7350
warisoncabral-rgb <warisoncabral@gmail.com>
2026-08-31 16:06:44 -0300

    feat: connect analyst shadow to Meta insights reader

--- backend/src/modules/analyst/analyst.module.ts
+++ backend/src/modules/analyst/analyst.module.ts
@@ -3,13 +3,14 @@ import { Pool } from 'pg';
 import { DatabaseModule } from '../../infrastructure/database/database.module';
 import { DATABASE_POOL } from '../../infrastructure/database/database.tokens';
 import { PostgresAnalystRepository } from '../../infrastructure/database/postgres-analyst.repository';
+import { MetaInsightsModule } from '../meta-insights/meta-insights.module';
 import { OperatorAccessModule } from '../operator-access/operator-access.module';
 import { AnalystController } from './analyst.controller';
 import { AnalystService } from './analyst.service';
 import { ANALYST_REPOSITORY } from './analyst.tokens';
 
 @Module({
-  imports: [DatabaseModule, OperatorAccessModule],
+  imports: [DatabaseModule, OperatorAccessModule, MetaInsightsModule],
   controllers: [AnalystController],
   providers: [
     {
